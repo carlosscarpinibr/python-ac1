b8bdb82701220c1ebb93cd192c27a0eebe21a33e
Caleb Mejia <jcalebm76@gmail.com>
2024-06-11 19:44:45 +0200

    menu caleb

--- script.py
+++ script.py
@@ -5,3 +5,17 @@ print('Teste del ordenador')
 print("Esto es una prueba")
 
 print('otra cosa')
+
+while True:
+    promt = ''' Menú Agencia de Viajes
+    1. Añadir destino
+    2. Mostrar destinos
+    3. Añadir cliente
+    4. Mostrar clientes
+    5. Realizar reserva
+    6. Cancelar reserva
+    7. Mostrar reservas
+    8. Salir
+    '''
+    print(promt)
+    opcion = input("Seleccione una opción: ")
