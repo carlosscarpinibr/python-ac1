0882ee2115799c6bb5941a6f04cabc952fad546b
Carlos Scarpini <21325368+carlosscarpinibr@users.noreply.github.com>
2024-06-11 20:03:45 +0200

    menu creado ok

--- script.py
+++ script.py
@@ -1,9 +1,40 @@
-print('Hola mundo!!')
+def principal():
+    prompt = '''    
+                    Menu - Agencia de viajes
+                    
+                1. Añadir un nuevo destino
+                2. Añadir un nuevo cliente
+                3. Realizar una reserva
+                4. Cancelar una reserva
+                5. Mostrar todos los destinos
+                6. Mostrar todos los clientes
+                7. Mostrar todas las reservas
+                0. Salir
+            '''
+    
+    
+    menu = 0
+    while menu != '0':
+        print(prompt)
+        menu=input('Entre una opción: ')
+        if menu == '1':
+            print('Añadir un nuevo destino')
+        elif menu == '2':
+            print('Añadir un nuevo cliente')
+        elif menu == '3':
+            print('Realizar una reserva')
+        elif menu == '4':
+            print('Cancelar una reserva')
+        elif menu == '5':
+            print('Mostrar todos los destinos')
+        elif menu == '6':
+            print('Mostrar todos los clientes')
+        elif menu == '7':
+            print('Mostrar todas las reservas')
+        elif menu == '0':
+            print('Fin del programa')
+        else:
+            print('Opción inválida, intente nuevamente.')
 
-print('Teste del ordenador')
-
-print("Esto es una prueba")
-
-print('otra cosa')
-
-print('teste')
+#Llama principal()
+principal()
