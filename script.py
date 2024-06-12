5641b89e8a5d626054440839e27f91b6d06aac89
Carlos Scarpini <21325368+carlosscarpinibr@users.noreply.github.com>
2024-06-12 10:37:52 +0200

    clear screen y pause screen añadido

--- script.py
+++ script.py
@@ -1,3 +1,14 @@
+import os
+
+def limpiar_pantalla():
+    os.system('clear') #clear screen mac
+    #os.system('cls') #clear screen windows
+    
+def pausar_pantalla():
+    print('Pulse Enter para continuar... ')
+    input()
+
+
 def nuevoDestino():
     codDest = input('Entra el codigo del destino: ')
     nomDest = input('Entra el nombre del destino: ')
@@ -47,31 +58,52 @@ def principal():
     while menu != '0':
         print(prompt)
         menu=input('Entre una opción: ')
+        limpiar_pantalla()
         if menu == '1':
             print('Añadir un nuevo destino')
             tablaDestinos.append(nuevoDestino())
+            print('Destino añadido corectamente.')
+            pausar_pantalla()
+            limpiar_pantalla()
         elif menu == '2':
             print('Añadir un nuevo cliente')
             tablaClientes.append(nuevoCliente())
+            print('Cliente añadido corectamente.')
+            pausar_pantalla()
+            limpiar_pantalla()
         elif menu == '3':
             print('Realizar una reserva')
             tablaReservas.append(nuevaReserva(tablaDestinos, tablaClientes))
+            print('Reserva añadida corectamente.')
+            pausar_pantalla()
+            limpiar_pantalla()
         elif menu == '4':
             print('Cancelar una reserva')
             tablaReservas.pop()
+            print('La reserva fue cancelada.')
+            pausar_pantalla()
+            limpiar_pantalla()
         elif menu == '5':
             print('Mostrar todos los destinos')
             print(tablaDestinos)
+            pausar_pantalla()
+            limpiar_pantalla()
         elif menu == '6':
             print('Mostrar todos los clientes')
             print(tablaClientes)
+            pausar_pantalla()
+            limpiar_pantalla()
         elif menu == '7':
             print('Mostrar todas las reservas')
             print(tablaReservas)
+            pausar_pantalla()
+            limpiar_pantalla()
         elif menu == '0':
             print('Fin del programa')
         else:
             print('Opción inválida, intente nuevamente.')
+            pausar_pantalla()
+            limpiar_pantalla()
 
 #Llama principal()
 principal()
