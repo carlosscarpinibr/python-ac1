d8841b6f65d3447e1a79d60528fe4b35ec6eed0d
Carlos Scarpini <21325368+carlosscarpinibr@users.noreply.github.com>
2024-06-13 02:24:28 +0200

    codigo listo y ayustado

--- script.py
+++ script.py
@@ -11,13 +11,13 @@ def pausar_pantalla():
 
 def nuevoDestino():
     while True:
-        codDest = input('Entra el codigo del destino: ')
+        codDest = input('Entra el codigo del destino: ').upper()
         if codDest == '':
             print('El campo no puede estar vacio, intente nuevamente.')
         else:
             break
     while True:
-        nomDest = input('Entra el nombre del destino: ')
+        nomDest = input('Entra el nombre del destino: ').upper()
         if nomDest == '':
             print('El campo no puede estar vacio, intente nuevamente.')
         else:
@@ -46,7 +46,7 @@ def nuevoCliente():
         except ValueError:
             print('Caractere inválido, intente nuevamente.')
     while True:       
-        nomCli = input('Entre el nombre del cliente: ')
+        nomCli = input('Entre el nombre del cliente: ').upper()
         if nomCli == '':
             print('El campo no puede estar vacio, intente nuevamente.')
         else:
@@ -62,7 +62,7 @@ def mostrar_clientes(tabla):
 
 def nuevaReserva(tablaDestino, tablaClientes):
         while True:
-            entraCod = input('Entra el código del destino: ')
+            entraCod = input('Entra el código del destino: ').upper()
             if entraCod == '':
                 print('El campo no puede estar vacio, intente nuevamente.')
             else:
@@ -79,19 +79,20 @@ def nuevaReserva(tablaDestino, tablaClientes):
             if entraCod == entradaDest['codigoDest']:
                 flag1 = True
                 entraDest = entradaDest['nombreDest']
+                entraPrec = entradaDest['precioDest']
         for entradaId in tablaClientes:
             if entraId == entradaId['idCliente']:
                 flag2 = True
                 entraCli = entradaId['nombreCli']
         if flag1 and flag2 == True:
-            reserva = {'codigoDestino' : entraCod , 'idCliente' : entraId , 'nombreCliente' : entraCli , 'nombreDestino' : entraDest}
+            reserva = {'codigoDestino' : entraCod , 'idCliente' : entraId , 'nombreCliente' : entraCli , 'nombreDestino' : entraDest , 'precioDest' : entraPrec}
             return True, reserva
         else:
             return False, None
 
 def cancelaReserva(tabla):
         while True:
-            entraCod = input('Entra el código del destino: ')
+            entraCod = input('Entra el código del destino: ').upper()
             if entraCod == '':
                 print('El campo no puede estar vacio, intente nuevamente.')
             else:
@@ -108,19 +109,20 @@ def cancelaReserva(tabla):
             if entraCod == entradaCod['codigoDestino']:
                 flag1 = True
                 entraDest = entradaCod['nombreDestino']
+                entraPrec = entradaCod['precioDest']
         for entradaId in tabla:
             if entraId == entradaId['idCliente']:
                 flag2 = True
                 entraCli = entradaId['nombreCliente']
         if flag1 and flag2 == True:
-            cancela = {'codigoDestino' : entraCod , 'idCliente' : entraId , 'nombreCliente' : entraCli , 'nombreDestino' : entraDest}
+            cancela = {'codigoDestino' : entraCod , 'idCliente' : entraId , 'nombreCliente' : entraCli , 'nombreDestino' : entraDest , 'precioDest' : entraPrec}
             return True, cancela
         else:
             return False, None                
 
 def mostrar_reservas(tabla):
     for entrada in tabla:
-        print(f'ID: {entrada['idCliente']} - Nombre: {entrada['nombreCliente']} - Destino: {entrada['codigoDestino']} - {entrada['nombreDestino']} ')
+        print(f'Reserva de {entrada['nombreCliente']}, destino: {entrada['nombreDestino']}({entrada['codigoDestino']}) por {entrada['precioDest']} €.')
 
 def verifica_lista(lista):
     if len(lista) == 0:
