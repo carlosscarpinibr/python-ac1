323d482a7f73bb50e00cbc2848ea7134cfb0d166
Albert <167766969+Riotplayer@users.noreply.github.com>
2024-06-13 17:10:28 +0200

    Introduciomos los comentarios oportunos en reserva

---
+++ help2.py
@@ -0,0 +1,6 @@
+from agenciaviaje.reserva.reservation import nuevaReserva
+from agenciaviaje.reserva.reservation import mostrar_reservas
+from agenciaviaje.reserva.reservation import cancelaReserva
+
+help(cancelaReserva)
+
