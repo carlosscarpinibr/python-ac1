46ddfd6bd93bcd74a5632f4a3614802099939ffc
Albert <167766969+Riotplayer@users.noreply.github.com>
2024-06-13 16:33:00 +0200

    Introducimos los comentarios en cliente.

--- agenciaviaje/cliente/client.py
+++ agenciaviaje/cliente/client.py
@@ -1,4 +1,10 @@
 def nuevoCliente():
+    '''
+    Descripcion: función para crear un nuevo cliente.
+    Entrada de datos: una variable entera para el ID del cliente
+      y un cadena de texto para el nombre del cliente.
+    Salida de datos: una variable del tipo diccionario, con los datos ingresados previamente.
+    '''
     while True:
         try:
             idCli = int(input('Entre un ID para el cliente: '))
@@ -17,5 +23,10 @@ def nuevoCliente():
     return cliente
 
 def mostrar_clientes(tabla):
+    '''
+    Descripcion: la función coteja los datos ingresados y los muestra en pantalla.
+    Entrada de datos: la lista de clientes del programa principal.
+    Salida de datos: mostramos la informacion de la lista por pantalla.
+    '''
     for entrada in tabla:
         print(f'ID: {entrada['idCliente']} - Nombre: {entrada['nombreCli']}')
---
+++ help.py
@@ -0,0 +1,5 @@
+from agenciaviaje.cliente.client import nuevoCliente
+from agenciaviaje.cliente.client import mostrar_clientes
+
+help(mostrar_clientes)
+ 
